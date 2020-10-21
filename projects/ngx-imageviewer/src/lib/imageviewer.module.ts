4513a7b7cdc2876e12feb14f410d26098f309ad9
Julind Mara <julind.mara@student.kit.edu>
2020-10-21 12:48:08 +0200

    Excplicitly import HammerJS
     + To fix movement of PDF/Images

--- projects/ngx-imageviewer/src/lib/imageviewer.module.ts
+++ projects/ngx-imageviewer/src/lib/imageviewer.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {ImageViewerComponent} from './imageviewer.component';
 import {IMAGEVIEWER_CONFIG, IMAGEVIEWER_CONFIG_DEFAULT} from './imageviewer.config';
+import {HammerModule} from '@angular/platform-browser';
 
 @NgModule({
   providers: [{provide: IMAGEVIEWER_CONFIG, useValue: IMAGEVIEWER_CONFIG_DEFAULT}],
   declarations: [ImageViewerComponent],
   exports: [ImageViewerComponent],
+  imports: [HammerModule]
 })
 export class ImageViewerModule {
 }
